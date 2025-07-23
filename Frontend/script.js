7f9d569818a93e522d6977ac20fb17a5f64d2d67
higher577 <youminkim577@gmail.com>
2025-07-23 18:01:50 +1200

    blur and close modal

--- Frontend/script.js
+++ Frontend/script.js
@@ -3,3 +3,10 @@ const login_btn = document.querySelector('.login-btn');
 login_btn.addEventListener('click', e=>{
     console.log('clicked');
 })
+
+function toggle() {
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
