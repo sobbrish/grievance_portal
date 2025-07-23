e54619d8b310744e4e67684fa230ce6bbf4a8459
sobbrish <AVeryTallPerson3m@gmail.com>
2025-07-23 20:09:34 +1200

    started routing

---
+++ Backend/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+
+// Setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '../Frontend')));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Fake login page
+app.get('/', (req, res) => {
+  res.render('index'); // login form page
+});
+
+// Route to show main page (after login)
+app.get('/main', (req, res) => {
+  res.render('main'); // only shown after login
+});
+
+// POST login: only Danica allowed
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === 'danica' && password === 'secret123') {
+    res.redirect('/main');
+  } else {
+    res.send('Access denied. Only Danica can use this site.');
+  }
+});
+
+
+app.listen(4000, () => {
+  console.log('Server running at http://localhost:4000');
+});
+
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
+
